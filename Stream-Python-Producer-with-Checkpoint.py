66f957951c1db72e6a83845d57e5d08c00c41747
eslam-mrc <eeemorsy@gmail.com>
2021-05-07 21:12:54 +0200

    undo changes in order of logic inside while loop

--- Stream-Python-Producer-with-Checkpoint.py
+++ Stream-Python-Producer-with-Checkpoint.py
@@ -59,40 +59,41 @@ while True:
     try:
         #tweets is an iterable object that contains stuff text of the tweet, who sent the tweet and date and more
         tweets = tw.Cursor(api.search, q=new_search, lang="en", since=date_since).items()
+        for tweet in tweets:
+            if tweet.id_str not in tweetsAlreadyRead:
+                verified = tweet.user.verified
+                try:
+                    ffRatio = round((int(tweet.user.followers_count)/int(tweet.user.friends_count)),0)
+                except:
+                    ffRatio = 0
+                #print(verified)
+                #print(ffRatio)
+                if ffRatio > 1 and verified == True:
+                    user_popularity = "Verified Celebrity"
+                elif ffRatio > 1 and verified == False:
+                    user_popularity = "Potential Celebrity"
+                elif ffRatio == 1:
+                    user_popularity = "Conversationalist"
+                else:
+                    user_popularity = "Spammer/Normal"
+                record = {"userID":tweet.user.id_str, "username":tweet.user.screen_name, "location":tweet.user.location, "followers":tweet.user.followers_count, "friends":tweet.user.friends_count, "statuses_count":tweet.user.statuses_count, "user_popularity":user_popularity, "tweetID":tweet.id_str, "tweetText":tweet.text, "tweetDate":tweet.created_at}
+                #print(record)
+                tweetsList.append(record)
+                tweetsAlreadyRead.append(tweet.id_str)
+                row = jsonpickle.encode(record)
+                sleep(2)
+                producer.send('playing', value=row)
+                print("===========Row sent===========")
+                with open('./checkpoint.txt', 'w') as filehandle:
+                    filehandle.writelines("%s\n" % line for line in tweetsAlreadyRead)
+                sleep(5)
     except tw.TweepError as error:
         print('There is a tweepy error with error code: ', error.api_code)    
         sleep(60 * 15)
         continue
     except StopIteration:
         break
-    for tweet in tweets:
-        if tweet.id_str not in tweetsAlreadyRead:
-            verified = tweet.user.verified
-            try:
-                ffRatio = round((int(tweet.user.followers_count)/int(tweet.user.friends_count)),0)
-            except:
-                ffRatio = 0
-            #print(verified)
-            #print(ffRatio)
-            if ffRatio > 1 and verified == True:
-                user_popularity = "Verified Celebrity"
-            elif ffRatio > 1 and verified == False:
-                user_popularity = "Potential Celebrity"
-            elif ffRatio == 1:
-                user_popularity = "Conversationalist"
-            else:
-                user_popularity = "Spammer/Normal"
-            record = {"userID":tweet.user.id_str, "username":tweet.user.screen_name, "location":tweet.user.location, "followers":tweet.user.followers_count, "friends":tweet.user.friends_count, "statuses_count":tweet.user.statuses_count, "user_popularity":user_popularity, "tweetID":tweet.id_str, "tweetText":tweet.text, "tweetDate":tweet.created_at}
-            #print(record)
-            tweetsList.append(record)
-            tweetsAlreadyRead.append(tweet.id_str)
-            row = jsonpickle.encode(record)
-            sleep(2)
-            producer.send('playing', value=row)
-            print("===========Row sent===========")
-            with open('./checkpoint.txt', 'w') as filehandle:
-                filehandle.writelines("%s\n" % line for line in tweetsAlreadyRead)
-            sleep(5)
+    
     
         
 
